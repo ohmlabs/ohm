9d4762eab6a7aea7c439f7b429de6d7ab3820660
Cameron Drake <cdrake757@gmail.com>
2013-10-11 14:05:10 -0700

    using the compass port of normalize over the submodule... easier

--- app/config/prod_config.rb
+++ app/config/prod_config.rb
@@ -1,5 +1,6 @@
 # Require any additional compass plugins here.
 require 'ceaser-easing'
+require 'compass-normalize'
 # Set this to the root of your project when deployed:
 http_path = ""
 css_dir = "www/css"
