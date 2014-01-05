599972284bb737ad96d970592ce3f0f6c203d280
Cameron Drake <cdrake757@gmail.com>
2014-01-04 17:30:24 -0800

    initial aws integration

---
+++ server/apis/AWS.js
@@ -0,0 +1,11 @@
+AWS = require('aws-sdk')
+// Configure AWS
+AWS.config.loadFromPath('./server/config/config.json');
+
+var s3 = new AWS.S3();
+s3.listBuckets(function(err, data) {
+  for (var index in data.Buckets) {
+    var bucket = data.Buckets[index];
+    console.log("Bucket: ", bucket.Name, ' : ', bucket.CreationDate);
+  }
+});
