231a6aa1296fe4258d61b9acb6ff12f4f38bc00d
Cameron Drake <cdrake757@gmail.com>
2014-01-06 19:37:01 -0800

    creating a parallax js

---
+++ client/js/parallax.js
@@ -0,0 +1,41 @@
+////////////////////////////////
+// Initialize                //
+//////////////////////////////
+$(document).ready(function(){
+  var skroll = skrollr.init({forceHeight:false, smoothScrolling:true});
+  //The options (second parameter) are all optional. The values shown are the default values.
+  skrollr.menu.init(skroll, {
+      //skrollr will smoothly animate to the new position using `animateTo`.
+      animate: true,
+  
+      //The easing function to use.
+      easing: 'sqrt',        
+  
+      //How long the animation should take in ms.
+      duration: function(currentTop, targetTop) {
+          //By default, the duration is hardcoded at 500ms.
+          return 500;
+  
+          //But you could calculate a value based on the current scroll position (`currentTop`) and the target scroll position (`targetTop`).
+          //return Math.abs(currentTop - targetTop) * 10;
+      },
+  });
+}); 
+
+////////////////////////////////
+// Parallax                  //
+//////////////////////////////
+$(window).scroll(function() {
+  parallax();
+});
+function parallax() {
+  // Get the scroll position of the page
+  scrollPos = $(this).scrollTop();
+  // Display Scroll position for debugging
+  $('#scrollTop').html($(this).scrollTop());
+  $('.logo').css({
+    'margin-top' : -(scrollPos/5)+"px", // Lower make the effect quicker
+    'opacity' : 1-(scrollPos/500), // Higher make the effect slower
+    'left' : 350 - scrollPos/5 +"px" // Lower make the effect quicker
+  });
+}
