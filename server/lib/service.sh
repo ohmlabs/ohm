22f137247a1697eb826ca5c129f1286965912518
Cameron Drake <cdrake757@gmail.com>
2014-01-31 09:42:19 +0000

    updating site to work on server

--- server/lib/service.sh
+++ server/lib/service.sh
@@ -1,43 +1,36 @@
 #! /bin/sh
-# /etc/init.d/boilerplate
+# /etc/init.d/drake
 #
 
-NAME=Boilerplate
+NAME=Drake.fm
 grunt=/usr/bin/grunt
 gem=/usr/bin/gem
 forever=/usr/bin/forever
 npm=/usr/bin/npm
 bower=/usr/bin/bower
-SITEROOT=~/boilerplate
-export PATH=$PATH:/usr/local/bin/
+SITEROOT=/home/cam/drake
+export PATH=$PATH:/usr/bin/
 
 case "$1" in
   start)
     echo "Starting $NAME"
     cd $SITEROOT
     pwd
-    $bower install
-    $npm install
-    $grunt
-    $forever start boilerplate.js
+    $grunt prod
+    $forever start drake.js -p
     ;;
   stop)
     echo "Stopping script $NAME"
     cd $SITEROOT
-    $forever stop boilerplate.js
+    $forever stop drake.js -p
 
     ;;
   install)
     echo "Beginning Installation for script $NAME"
     cd $SITEROOT
-    # install global node modules first https://npmjs.org/ 
-    sudo $npm install -g bower grunt-cli forever coffee-script node-inspector
-    # install Ruby gems
-    sudo $gem install sass compasss ceaser-easing normalize
-    # install node modules
+    $npm cache clean
     $npm install
-    # install client dependencies http://bower.io/
-    $bower install
+    $bower install --allow-root
     git submodule init
     git submodule update
 
@@ -47,7 +40,7 @@ case "$1" in
     $forever list
     ;;
   *)
-    echo "Usage: /etc/init.d/boilerplate {start|stop|list}"
+    echo "Usage: /etc/init.d/drake {start|stop|list}"
     exit 1
     ;;
 esac
