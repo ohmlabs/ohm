e8d70dbb10056c8e44cbcec51f7b6a8fe56fa4f3
Cameron Drake <cdrake757@gmail.com>
2014-01-30 18:14:13 -0800

    baller ass install process on dev son fix #2 and fix #12

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
-gem=/usr/bin/gem
-npm=/usr/bin/npm
-grunt=/usr/bin/grunt
+gem=~/.rvm/bin/gem
+npm=/usr/local/bin/npm
+git=/usr/local/bin/git
+bower=/usr/local/share/npm/bin/bower
+grunt=/usr/local/share/npm/bin/grunt
 NGINX_CONFIG=/etc/nginx/
 GIT=~/git/boilerplate.git
 FILE_DIR=`pwd`
@@ -12,37 +14,48 @@ function doIt() {
         sudo ln -s $FILE_DIR/server/lib/service.sh /etc/init.d/boilerplate
         sudo chmod 0755 /etc/init.d/boilerplate
         cd $NGINX_CONFIG
-	pwd
+  pwd
         sudo ln -s $FILE_DIR/server/lib/nginx.conf .
         sudo ln -s $FILE_DIR/server/lib/sites-available sites-enabled
         cd $GIT
-	pwd
+  pwd
         ln -s $FILE_DIR/server/lib/hooks/ hooks
-	cd hooks
-	ls -la
+  cd hooks
+  ls -la
         sudo chmod +x pre-receive.sh
         sudo chmod +x post-receive.sh
-	cd "$(dirname "${BASH_SOURCE}")"
-	pwd
-        # install gloabal node modules
-        sudo $npm install -g bower grunt-cli forever coffee-script node-inspector
-        sudo $gem install sass compasss ceaser-easing normalize
+  cd "$(dirname "${BASH_SOURCE}")"
+  pwd
+        # install 
+  sudo service boilerplate install
+  sudo service boilerplate start
 }
 function dev() {
         cd "$(dirname "${BASH_SOURCE}")"
-        # install gloabal node modules
-        $npm install -g bower grunt-cli forever coffee-script node-inspector
-        $gem install sass compasss ceaser-easing normalize
+  # install global node modules first https://npmjs.org/ 
+  sudo $npm install -g bower grunt-cli forever coffee-script node-inspector
+  # install Ruby gems
+  sudo $gem install sass compass ceaser-easing normalize
+  # install node modules 
+  $npm install
+  # install client dependencies http://bower.io/
+  $bower install
+  $git submodule init
+  $git submodule update
+  $grunt forever:start
+  tail -n 1 logs/node-bp.log
+  
 }
 if [ "$1" == "--force" -o "$1" == "-f" ]; then
         doIt
 else
-        read -p "This may overwrite existing files in your home directory. What type of install is this? (p)Prod (d)Dev enter anything else to cancel" -n 1
+        read -p "This may overwrite existing files in your home directory. What type of install is this? (p)Prod (d)Dev" -n 1
         echo
         if [[ $REPLY =~ ^[Pp]$ ]]; then
                 doIt
-	elif [[ $REPLY =~ ^[Dd]$ ]]; then
+        elif [[ $REPLY =~ ^[Dd]$ ]]; then
                 dev
-	fi
+        fi
 fi
 unset doIt
+unset dev
