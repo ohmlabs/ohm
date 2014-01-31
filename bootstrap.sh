d203af743cc5287160ed6721ed92cd3e0842056a
Cameron Drake <cdrake757@gmail.com>
2014-01-31 01:03:29 +0000

    fixed the boostrap to prompt for dev or prod

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,22 +1,34 @@
-#! /bin/sh
+#!/bin/bash
 gem=/usr/bin/gem
 npm=/usr/bin/npm
 grunt=/usr/bin/grunt
 NGINX_CONFIG=/etc/nginx/
 GIT=~/git/boilerplate.git
-
+FILE_DIR=`pwd`
+echo $FILE_DIR
 cd "$(dirname "${BASH_SOURCE}")"
 git pull origin master
 function doIt() {
-        ln -s server/lib/service.sh /etc/init.d/boilerplate
-        chmod 0755 /etc/init.d/boilerplate
+        sudo ln -s $FILE_DIR/server/lib/service.sh /etc/init.d/boilerplate
+        sudo chmod 0755 /etc/init.d/boilerplate
         cd $NGINX_CONFIG
-        ln -s server/lib/nginx.conf .
-        ln -s server/lib/sites-available sites-enabled
+	pwd
+        sudo ln -s $FILE_DIR/server/lib/nginx.conf .
+        sudo ln -s $FILE_DIR/server/lib/sites-available sites-enabled
         cd $GIT
-        ln -s server/lib/hooks/* hooks
-        chmod +x hooks/pre-receive
-        chmod +x hooks/post-receive
+	pwd
+        ln -s $FILE_DIR/server/lib/hooks/ hooks
+	cd hooks
+	ls -la
+        sudo chmod +x pre-receive.sh
+        sudo chmod +x post-receive.sh
+	cd "$(dirname "${BASH_SOURCE}")"
+	pwd
+        # install gloabal node modules
+        sudo $npm install -g bower grunt-cli forever coffee-script node-inspector
+        sudo $gem install sass compasss ceaser-easing normalize
+}
+function dev() {
         cd "$(dirname "${BASH_SOURCE}")"
         # install gloabal node modules
         $npm install -g bower grunt-cli forever coffee-script node-inspector
@@ -25,10 +37,12 @@ function doIt() {
 if [ "$1" == "--force" -o "$1" == "-f" ]; then
         doIt
 else
-        read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1
+        read -p "This may overwrite existing files in your home directory. What type of install is this? (p)Prod (d)Dev enter anything else to cancel" -n 1
         echo
-        if [[ $REPLY =~ ^[Yy]$ ]]; then
+        if [[ $REPLY =~ ^[Pp]$ ]]; then
                 doIt
-        fi
+	elif [[ $REPLY =~ ^[Dd]$ ]]; then
+                dev
+	fi
 fi
 unset doIt
