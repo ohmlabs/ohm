c5b99c3d44f511dd5493f8373b94d90409912bcc
Cameron Drake <cdrake757@gmail.com>
2014-01-31 21:27:03 -0800

    adding a deploy script to install hooks separate from bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 gem=~/.rvm/bin/gem
-npm=/usr/local/bin/npm
+npm=/usr/bin/npm
 git=/usr/local/bin/git
 bower=/usr/local/share/npm/bin/bower
-grunt=/usr/local/share/npm/bin/grunt
 NGINX_CONFIG=/etc/nginx/
 GIT=~/git/boilerplate.git
 FILE_DIR=`pwd`
@@ -13,43 +12,39 @@ git pull origin master
 function doIt() {
         ln -is $FILE_DIR/.gitconfig ~
         ln -is $FILE_DIR/.zshrc  ~
-        sudo ln -is $FILE_DIR/server/lib/service.sh /etc/init.d/drake
-        sudo chmod 0755 /etc/init.d/drake
+        sudo ln -is $FILE_DIR/server/lib/service.sh /etc/init.d/boilerplate
+        sudo chmod 0755 /etc/init.d/boilerplate
         cd $NGINX_CONFIG
         pwd
         sudo ln -is $FILE_DIR/server/lib/nginx.conf .
         sudo ln -is $FILE_DIR/server/lib/sites-available sites-enabled
-        cd $GIT
-        pwd
-        ln -is $FILE_DIR/server/lib/hooks/ hooks
-        cd hooks
-        sudo chmod +x pre-receive.sh
-        sudo chmod +x post-receive.sh
         cd "$(dirname "${BASH_SOURCE}")"
         pwd
-        # install global node modules first https://npmjs.org/
+        # install global node modules first https://npmjs.org/ cache clean just in case
         sudo $npm cache clean
-	sudo $npm install -g bower grunt-cli forever coffee-script node-inspector
+        sudo $npm install -g bower grunt-cli forever coffee-script node-inspector
         # install Ruby gems
         sudo $gem install sass compasss ceaser-easing normalize
         sudo service boilerplate install
         sudo service boilerplate start
 }
 function dev() {
-        cd "$(dirname "${BASH_SOURCE}")"
-  	# install global node modules first https://npmjs.org/ 
-  	sudo $npm install -g bower grunt-cli forever coffee-script node-inspector
-  	# install Ruby gems
-  	sudo $gem install sass compass ceaser-easing normalize
-  	# install node modules 
-  	$npm install
-  	# install client dependencies http://bower.io/
-  	$bower install
-  	$git submodule init
-  	$git submodule update
-  	$grunt forever:start
-  	tail -n 1 logs/node-bp.log
-  
+    cd "$(dirname "${BASH_SOURCE}")"
+    ln -is $FILE_DIR/.gitconfig ~
+    ln -is $FILE_DIR/.sshconfig  ~/.ssh/config
+    ln -is $FILE_DIR/.zshrc  ~
+    source ~/.zshrc
+    # install global node modules first https://npmjs.org/ 
+    sudo $npm install -g bower grunt-cli forever coffee-script node-inspector
+    # install Ruby gems
+    sudo $gem install sass compass ceaser-easing normalize
+    # install node modules 
+    $npm install
+    # install client dependencies http://bower.io/
+    $bower install
+    $git submodule init
+    $git submodule update
+    tail -n 1 logs/node-bp.log
 }
 if [ "$1" == "--force" -o "$1" == "-f" ]; then
         doIt
