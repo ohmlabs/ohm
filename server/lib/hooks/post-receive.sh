87cd409de6d42bb9b19f55faa43b212684bc1ede
Cameron Drake <cdrake757@gmail.com>
2014-01-31 20:02:51 -0800

    one last try

--- server/lib/hooks/post-receive.sh
+++ server/lib/hooks/post-receive.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 echo "Checkout Updates"
-GIT_WORK_TREE=/home/cam/drake git checkout master -f
+GIT_WORK_TREE=/home/ubuntu/drake git checkout master -f
 echo "start drake server"
 sudo service drake install
 sudo service drake compile
