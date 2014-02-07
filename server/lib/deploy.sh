71b2df71a12ef714b73afed56c182e6647d0a327
Cameron Drake <cdrake757@gmail.com>
2014-02-07 15:01:31 -0800

    updating shell files

--- server/lib/deploy.sh
+++ server/lib/deploy.sh
@@ -8,11 +8,11 @@ WORK_TREE=~/production
 
 mkdir -p $GIT && cd $GIT
 $git --bare init
-git --bare fetch git@github.com:cdrake757/boilerplate.git master:master
+git --bare fetch git@github.com:ohmlabs/ohmlabs.git master:master
 sudo cp -i $FILE_DIR/hooks/pre-receive.sh $GIT/hooks/pre-receive
 sudo cp -i $FILE_DIR/hooks/post-receive.sh $GIT/hooks/post-receive
 sudo chmod +x $GIT/hooks/pre-receive
 sudo chmod +x $GIT/hooks/post-receive
-sudo cp -i $FILE_DIR/service.sh /etc/init.d/jukeboxP
-sudo chmod 0755 /etc/init.d/jukeboxP
+sudo cp -i $FILE_DIR/service.sh /etc/init.d/ohmlabs
+sudo chmod 0755 /etc/init.d/ohmlabs
 mkdir $WORK_TREE
