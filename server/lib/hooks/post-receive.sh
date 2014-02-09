589d4694012ff58947d30e040d082ac99ffc8696
Cameron Drake <cdrake757@gmail.com>
2014-02-09 00:30:22 -0800

    updating post hook

--- server/lib/hooks/post-receive.sh
+++ server/lib/hooks/post-receive.sh
@@ -2,8 +2,6 @@
 echo "Checkout Updates"
 GIT_WORK_TREE=~/drake git checkout master -f
 cd ~/drake
-git submodule init 
-git submodule update
 echo "start drake server"
 sudo service drake install
 sudo service drake start
