c8ece26e0074031cca12b8196b00b94493746f55
Cameron Drake <cameron@drake.fm>
2014-04-02 08:03:49 -0700

    removing deploy

--- deploy/production/hooks/post-receive.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-echo "Checkout Updates"
-GIT_WORK_TREE=/home/git/drake git checkout master -f
-echo "start drake server"
-sudo service drake install
-sudo service drake start
--- deploy/production/hooks/pre-receive.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-echo "Stopping service"
-sudo service drake stop
