18bdfa96aefc18c3b6633d8b28f518d6690dfbbe
Cameron Drake <cameron@drake.fm>
2014-06-27 15:16:25 -0700

    update

--- server/ghost/config.js
+++ server/ghost/config.js
@@ -45,7 +45,7 @@ config = {
     // When running Ghost in the wild, use the production environment
     // Configure your URL and mail settings here
     production: {
-        url: 'http://weiss.drake.fm',
+        url: 'http://blog.drake.fm',
         mail: {},
         database: {
             client: 'sqlite3',
