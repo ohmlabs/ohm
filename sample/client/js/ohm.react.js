bc77b97c075c501522ff14e2a86a79df2a7b8a84
Cameron Drake <cdrake@chooseenergy.com>
2016-10-28 20:24:52 -0700

    rearrange

--- sample/client/js/ohm.react.js
+++ sample/client/js/ohm.react.js
@@ -1,11 +1,11 @@
 (function () {
   'use strict';
-  const styles   = require('./../sass/ohm.sass');
+  const styles   = require('./../sass/style.sass');
   const React    = require('react');
   const ReactDOM = require('react-dom');
   const io       = require('socket.io-client');
-  const LightBox = require('./ohm/LightBox.react.js');
-  const GMap     = require('./ohm/GMapper.react.js');
+  const LightBox = require('./../../../ohm/client/js/lib/LightBox.react.js');
+  const GMap     = require('./../../../ohm/client/js/lib/GMapper.react.js');
   const socket   = io.connect();
 
   socket.on('connect', (data) => {
--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path       = require('path');
 const webpack    = require("webpack");
 module.exports = [{
   entry: {
-    ohm: './client/js/ohm.react.js'
+    ohm: './sample/client/js/ohm.react.js'
   },
   context: __dirname,
   output: {
@@ -15,13 +15,15 @@ module.exports = [{
       exclude: /(node_modules|bower_components)/,
       loader: 'babel-loader',
       query: {
-        presets: ['es2015', 'react']
+        presets: ['es2015', 'react'],
+        cacheDirectory: './.babel-loader/'
       }
     }, {
       test: /\.sass$/,
       loader: 'style-loader!raw-loader!sass-loader'
         + '?includePaths[]=' + path.resolve(__dirname, './node_modules/compass-mixins/lib')
         + '&includePaths[]=' + path.resolve(__dirname, './node_modules/bootstrap-sass/assets/stylesheets')
+        + '&includePaths[]=' + path.resolve(__dirname, './ohm/client/sass/')
     }]
   }
 }];
