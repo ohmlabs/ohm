03003cf1ee80f54e7287b4b192d72b4875d808dc
Cameron Drake <cdrake@chooseenergy.com>
2016-11-10 14:08:31 -0800

    coverage tools and exporting ViewerContext

--- Gruntfile.js
+++ Gruntfile.js
@@ -45,6 +45,24 @@ module.exports = function(grunt) {
       }
     },
 
+    coveralls: {
+      // Options relevant to all targets
+      options: {
+        // When true, grunt-coveralls will only print a warning rather than
+        // an error, to prevent CI builds from failing unnecessarily (e.g. if
+        // coveralls.io is down). Optional, defaults to false.
+        force: false
+      },
+
+      your_target: {
+        // LCOV coverage file (can be string, glob or array)
+        src: 'coverage/extra-results-*.info',
+        options: {
+          // Any options for just this target
+        }
+      },
+    },
+
     copy: {
       main: {
         expand: true,
