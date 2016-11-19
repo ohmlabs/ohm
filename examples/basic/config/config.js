f13c74ec49202b586ed3e0c19238407384259715
Cameron Drake <cdrake@chooseenergy.com>
2016-11-18 16:12:01 -0800

    config fixes

--- examples/basic/config/config.js
+++ examples/basic/config/config.js
@@ -12,9 +12,7 @@
       env             : 'production',
       host            : 'localhost',
       port            : '8888',
-      MONGO_PORT      : '27017',
       MONGO_HOST      : 'mongo',
-      REDIS_PORT      : '6379',
       REDIS_HOST      : 'redis',
     };
     break;
@@ -23,35 +21,11 @@
       env             : 'development',
       host            : 'localhost',
       port            : '8888',
-      MONGO_PORT      : '27017',
-      MONGO_HOST      : 'localhost',
-      REDIS_PORT      : '6379',
-      REDIS_HOST      : 'localhost',
     };
   }
   _.extend(base, {
-    // AWS
-    AWS_ACCESS_KEY            : 'OHMTEST',
-    AWS_SECRET_KEY            : 'OHMTEST',
-    // MongoDB instance
-    MONGODB_INSTANCE          : 'mongodb://' + base.MONGO_HOST + ':' + base.MONGO_PORT + '/' + base.MONGO_DB,
-    // Parse
-    PARSE_SERVER_URL          : 'http://' + base.host + ':' + base.port+ '/parse',
-    PARSE_APPLICATION_ID      : 'OHMTEST',
-    PARSE_JAVASCRIPT_KEY      : 'OHMTEST',
-    PARSE_MASTER_KEY          : 'OHMTEST',
-    // Socket.io
-    SOCKETIO_SESSION_SECRET   : 'OHMTEST',
-    // This is the main account for ViewerContext
-    PRIMARY_ACCOUNT_KEY       : '',
-    // Custom Config
-    PARSE_PATH                : '/parse',
-    VIEWS_DIR                 : '',
     ROUTES                    : path.join(__dirname, '../routes/site.js'),
     SOCKETS                   : path.join(__dirname, '../routes/sockets.js'),
-    SESSION_KEY               : 'ojxsid',
-    MONGO_DB                  : 'ohm',
-    AWS_BUCKET_NAME           : 'ohmdev',
   });
 
   module.exports = base;
