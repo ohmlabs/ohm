fa661302ae8815dc4a73c9f66ebf5e906fe2153e
Cameron Drake <cdrake@chooseenergy.com>
2016-11-23 17:28:03 -0800

    no ghost for basic

--- examples/basic/config/config.js
+++ examples/basic/config/config.js
@@ -24,10 +24,8 @@
     };
   }
   _.extend(base, {
-    ROUTES                    : path.join(__dirname, '../routes/site.js'),
-    GHOST_PATH                : '/blog',
-    GHOST_CONFIG_PATH         : path.join(__dirname, './ghost.config.js'),
-    SOCKETS                   : path.join(__dirname, '../routes/sockets.js'),
+    ROUTES            : path.join(__dirname, '../routes/site.js'),
+    SOCKETS           : path.join(__dirname, '../routes/sockets.js'),
   });
 
   module.exports = base;
